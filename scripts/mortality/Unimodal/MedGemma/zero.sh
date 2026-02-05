593361ea1d46ed2d1cf1450a80026ab1d11252f6
Baraa Al Jorf <baj321@nyu.edu>
2026-02-05 08:11:40 +0400

    Add files via upload

---
+++ scripts/mortality/Unimodal/MedGemma/zero.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH -p nvidia
+#SBATCH --gres=gpu:a100:1
+#SBATCH --time=3-08:59:59
+#SBATCH --cpus-per-task=18
+# Output and error files
+#SBATCH -o final_outlogs/job.%J.out
+#SBATCH -e final_errlogs/job.%J.err
+
+# Set the Hugging Face cache directory to your scratch space
+export HF_HOME="/data/user/.cache/huggingface"
+
+# Set your Hugging Face authentication token
+export HUGGING_FACE_HUB_TOKEN="REDACTED_TOKEN"
+    
+# Activating conda
+eval "$(conda shell.bash hook)"
+conda activate llama3_env
+
+python agent_main.py \
+    --data_path "/data/user/MedAgent/datasets/multimodal_dataset_splits/test.jsonl" \
+    --few_shot_data_path "/data/user/MedAgent/datasets/multimodal_dataset_splits/train.jsonl" \
+    --train_data_path "/data/user/MedAgent/datasets/multimodal_dataset_splits/train.jsonl" \
+    --mimic_notes_dir "scratch/baj321/MIMIC-Note/physionet.org/files/mimic-iv-note/2.2/note/" \
+    --modalities "ps" \
+    --agent_setup "SingleAgent" \
+    --model_id "google/medgemma-4b-it" \
+    --output_dir "./results/medgemma/zeroshot-ps" \
+    --batch_size 3 \
+    --num_shots 2 \
+    --refine_iterations 1 \
+    --debate_rounds 3 \
+    --debug_samples 2
